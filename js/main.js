d9205e176ef44623691971de62d7c023ca95ef73
Daniel338 <danielalejo131@gmail.com>
2021-03-22 12:07:54 -0300

    Crea los elementos html en el js y se muestra en el row-1

--- js/main.js
+++ js/main.js
@@ -40,3 +40,51 @@ function mostrarNote()
         typeNote.classList.remove('active');
     }
 }
+// New Note
+const noteInput = document.getElementById('nota');
+noteInput.addEventListener('click', newNote, false);
+
+function newNote()
+{
+
+	let noteTitle = prompt('ingrese el titulo de la nota:');
+	let noteContent = prompt('Ingrese el contenido de la nota:');
+	let row_1 = document.getElementById('box_content');
+
+	// Box model
+	let boxArticle = document.createElement('article');
+	boxArticle.className = 'box';
+
+	const box__top = document.createElement('div');
+	box__top.className = 'box__top';
+
+	const box__bottom = document.createElement('div');
+	box__bottom.className = 'box__bottom';
+
+	const icon = document.createElement('span');
+	const ico = document.createElement('i');
+	const box_subtitle = document.createElement('h4');
+	box_subtitle.textContent = `${noteContent}`;
+	box_subtitle.className = 'box-subtitle';
+	icon.className = 'icon';
+	ico.className = 'far fa-star';
+
+	/// Anidando elementos de Box__top
+	icon.appendChild(ico);
+	box__top.appendChild(icon);
+	box__top.appendChild(box_subtitle);
+
+
+	const boxTitle = document.createElement('h3');
+	boxTitle.className = 'box__title';
+	boxTitle.textContent = `${noteTitle}`;
+
+	noteOn = false;
+    typeNote.classList.remove('active');
+	
+	boxArticle.appendChild(box__top);
+	boxArticle.appendChild(boxTitle);
+	boxArticle.appendChild(box__bottom);
+	row_1.appendChild(boxArticle);
+	console.log(boxArticle);
+}
