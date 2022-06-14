d7c941ef3b15eaf30a4e8aacc75a82e1be18fc13
daniel338 <danielalejo131@gmail.com>
2022-06-14 07:45:19 -0300

    add function saveNote

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 
-// Compenzar el script cuando carge el DOM
+// Comenzar el script cuando carge el DOM
 document.addEventListener('DOMContentLoaded', development, false);
 
-// Variables
+// Variables globales
 
 // Scripts
 function development ()
 {
 	console.log('Page load complete');
 	
-	// Variables
+	// Variables locales
 	const btnPlus = document.getElementById('btnPlus');
 	const typeNote = document.getElementById('typeNote');
 	const btnNote = document.getElementById('btnNote');
 	const noteRight = document.getElementById('note__right');
 	const inputTitle = document.getElementById('noteTitle');
 	const inputContent = document.getElementById('noteContent');
+    const inputSave = document.querySelector('#inputSave');
 
+    // Ventana de notas
 	function inputNotes()
 	{
 		noteRight.classList.add('active');
+        inputTitle.focus();
+        inputSave.addEventListener('click', saveNote, false);
 	}
+
+    function saveNote()
+    {
+        let title = inputTitle.value;
+        let content = inputContent.value;
+
+        console.log(`El titulo es: \n${title}`);
+        console.log(`El contenido es: \n${content}`);
+    }
 
 	btnPlus.onclick = function()
 	{
